d0a7889a92b63241f3fa26f3977be1a76a787f8f
anoopms1988 <1988anoopms@gmail.com>
2017-10-30 05:18:29 +0530

    Add bookinstance

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var bodyParser = require('body-parser');
 var index = require('./routes/index');
 var users = require('./routes/users');
 var catalog = require('./routes/catalog');
+var expressValidator = require('express-validator');
 
 var app = express();
 
@@ -31,6 +32,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(expressValidator())
 
 app.use('/', index);
 app.use('/users', users);
